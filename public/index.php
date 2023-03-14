4b86ac23bf105e44f0ece42322ebbee1b3400b97
Borys Lebeda <Borys_Lebeda@epam.com>
2023-03-13 23:12:08 -0700

    Add Twig

--- public/index.php
+++ public/index.php
@@ -2,14 +2,28 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+// Create Twig
+$twig = Twig::create('./templates',
+    [
+        'cache' => '/../var/cache',
+        'auto_reload' => true
+    ]);
+
+// $twig->addExtension(new IntelExtension());
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::create($app, $twig));
+
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'index.twig');
 });
 
 $app->run();
